fd8e40afdc0532a58e17c1eaf46cb81a566e62ec
RollemIra <rollemira@users.noreply.github.com>
2024-03-11 00:06:03 -0400

    theme stuff

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,13 +1,8 @@
 return {
   {
-    "catppuccin/nvim",
-    lazy = false,
-    name = "catppuccin",
-    priority = 1000,
-  },
-  {
     "LazyVim/LazyVim",
     opts = {
+      --colorscheme = "tokyonight",
       --colorscheme = "tokyonight-storm",
       colorscheme = "catppuccin",
     },
