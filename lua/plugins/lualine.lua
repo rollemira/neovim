64b4ebe240ac6a61d68bb8b2cf6bf6b0918155de
RollemIra <rollemira@users.noreply.github.com>
2024-03-11 00:31:15 -0400

    lualine dracula with pills in bar

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,6 +4,20 @@ return {
     require("lualine").setup({
       options = {
         theme = "dracula",
+        component_separators = "",
+        section_separators = { left = "", right = "" },
+      },
+      sections = {
+        lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
+        lualine_z = {
+          {
+            "datetime",
+            -- options: default, us, uk, iso, or your own format string ("%H:%M", etc..)
+            style = "%H:%M",
+            separator = { right = "" },
+            left_padding = 2,
+          },
+        },
       },
     })
   end,
